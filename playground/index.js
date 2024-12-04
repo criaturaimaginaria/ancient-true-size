af38b3de892faa30d52bf0f73b7291730a8e0324
Valentin <valkjavalkja@gmail.com>
2024-12-04 16:14:39 -0300

    first commit

---
+++ playground/index.js
@@ -0,0 +1,204 @@
+const options = {
+  stroke: true,
+  weight: 2,
+  lineJoin: 'round',
+  opacity: 1,
+};
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const Playground = {
+  init(_containerID, _layers, _options = {}) {
+    this._container = document.querySelector(_containerID);
+    this._map = this.mountMap(this._container);
+    this._layers = _layers; // Guardar todas las capas disponibles
+    this._activeLayers = []; // Mantener las capas activas
+    this._plugins = {}; // Asociar nombres con plugins
+
+    this.createLayerControls();
+    this.loadInitialLayer(); // Mostrar solo una capa inicialmente
+
+    // Cargar las capas desde el archivo geojson
+    this.loadGeoJsonData();
+  },
+
+  mountMap(_container) {
+    const map = L.map(_container, {
+      center: [40, 60],
+      zoom: 3,
+      zoomControl: false,
+      zoomDelta: 0.25,
+      zoomSnap: 0.25,
+    });
+
+    new L.tileLayer(
+      'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png',
+      {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+        detectRetina: true,
+      }
+    ).addTo(map);
+
+    return map;
+  },
+
+  mountPlugin(_map, _layerName, _data, _options) {
+    const color = getRandomColor(); // Obtener un color aleatorio
+    const layerOptions = { 
+      ..._options, 
+      color, // Añadir color a las opciones de la capa
+      fillColor: color, // Aplicar el mismo color al relleno
+      fillOpacity: 0.5 // Ajustar la opacidad del relleno si es necesario
+    };
+    
+    // Verificar el tipo de capa y crearla
+    let plugin;
+    if (_data.type === 'Polygon' || _data.type === 'MultiPolygon') {
+      plugin = L.geoJSON(_data, {
+        style: {
+          weight: layerOptions.weight,
+          color: layerOptions.color, // Aplicar el color aleatorio al borde
+          opacity: layerOptions.opacity,
+          lineJoin: layerOptions.lineJoin,
+          fillColor: layerOptions.fillColor, // Aplicar el color al relleno
+          fillOpacity: layerOptions.fillOpacity // Configurar la opacidad del relleno
+        },
+      }).addTo(_map);
+    } else {
+      plugin = new L.TrueSize(_data, layerOptions).addTo(_map);
+    }
+
+    this._plugins[_layerName] = plugin; // Guardar referencia del plugin
+    this._activeLayers.push(_layerName); // Añadir a las capas activas
+    return plugin;
+  },
+
+
+  loadInitialLayer() {
+    const initialLayerName = Object.keys(this._layers)[0];
+    const initialLayerData = this._layers[initialLayerName];
+  
+    this.mountPlugin(this._map, initialLayerName, initialLayerData, options);
+  
+    const activeLayersList = document.querySelector('#active-layers');
+    // const layerNameFromProperties = initialLayerData.properties?.name || initialLayerName;
+    const layerNameFromProperties = initialLayerData.properties?.name;
+  
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <span>${layerNameFromProperties}</span>
+      <button data-layer="${initialLayerName}" class="hide-layer">Hidde</button>
+      <button data-layer="${initialLayerName}" class="remove-layer">Delete</button>
+    `;
+    activeLayersList.appendChild(listItem);
+  },
+
+  createLayerControls() {
+    const searchInput = document.querySelector('#search-input');
+    const layerSelect = document.querySelector('#layer-select');
+    const activeLayersList = document.querySelector('#active-layers');
+
+
+    searchInput.addEventListener('input', (event) => {
+      const searchTerm = event.target.value.toLowerCase(); // Convertir a minúsculas para coincidencias insensibles a mayúsculas
+      const options = layerSelect.querySelectorAll('option');
+    
+      options.forEach((option) => {
+        const layerName = option.textContent.toLowerCase();
+        // Mostrar solo las opciones que coincidan con el término de búsqueda
+        if (layerName.includes(searchTerm)) {
+          option.style.display = '';
+        } else {
+          option.style.display = 'none';
+        }
+      });
+    });
+  
+    for (const [layerName, layerData] of Object.entries(this._layers)) {
+      const option = document.createElement('option');
+      option.value = layerName;
+      option.textContent = layerData.properties.name;
+      layerSelect.appendChild(option);
+    }
+
+    layerSelect.addEventListener('change', (event) => {
+      const layerName = event.target.value;
+      const layerData = this._layers[layerName];
+  
+      if (!this._activeLayers.includes(layerName)) {
+        this.mountPlugin(this._map, layerName, layerData, options);
+  
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+          <span>${layerData.properties.name}</span>
+          <button data-layer="${layerName}" class="hide-layer">Hidde</button>
+          <button data-layer="${layerName}" class="remove-layer">Delete</button>
+        `;
+        activeLayersList.appendChild(listItem);
+      }
+    });
+
+    activeLayersList.addEventListener('click', (event) => {
+      const layerName = event.target.getAttribute('data-layer');
+  
+      if (event.target.classList.contains('hide-layer')) {
+        this.toggleLayerVisibility(layerName);
+      } else if (event.target.classList.contains('remove-layer')) {
+        this.removeLayer(layerName, event.target.parentElement);
+      }
+    });
+
+
+
+  },
+
+  toggleLayerVisibility(layerName) {
+    const plugin = this._plugins[layerName];
+    if (this._map.hasLayer(plugin)) {
+      this._map.removeLayer(plugin);
+    } else {
+      plugin.addTo(this._map);
+    }
+  },
+
+  removeLayer(layerName, listItem) {
+    const plugin = this._plugins[layerName];
+    this._map.removeLayer(plugin);
+    delete this._plugins[layerName];
+    this._activeLayers = this._activeLayers.filter((name) => name !== layerName);
+    listItem.remove();
+  },
+};
+
+const layers = {
+  // jsondata: geojsonData,
+  RomeTrajano: RomeTrajano,
+  RomeKingdom1: RomeKingdom1,
+  RomeKingdom2: RomeKingdom2,
+  romeAppClaudius: romeAppClaudius,
+  Venice700: Venice700,
+  Venice1500: Venice1500,
+  Amalfi: Amalfi,
+  Athens: Athens,
+  EgyptOldKingdom: EgyptOldKingdom,
+  egyptFirstIntermediate1: egyptFirstIntermediate1,
+  egyptFirstIntermediate2: egyptFirstIntermediate2,
+  egyptMiddleKingdom: egyptMiddleKingdom,
+  egyptNewKingdom: egyptNewKingdom,
+
+  // jsondata: europeSquare,
+  polygon: polygon,
+};
+
+Playground.init('#map', layers, options);
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,247 @@
+// import L from 'leaflet';
+// import turfBearing from '@turf/bearing';
+// import turfDistance from '@turf/distance';
+// import turfDestination from '@turf/destination';
+// import { coordAll as turfCoordAll } from '@turf/meta';
+
+// let id = 0;
+
+// L.TrueSize = L.Layer.extend({
+//   geoJSON: {
+//     type: 'Feature',
+//     properties: {},
+//     geometry: {
+//       type: 'Polygon',
+//       coordinates: []
+//     }
+//   },
+
+//   options: {
+//     color: '#FF0000',
+//     stroke: true,
+//     weight: 1,
+//     opacity: 1,
+//     lineCap: 'round',
+//     lineJoin: 'round',
+//     dashArray: null,
+//     dashOffset: null,
+//     fill: true,
+//     fillColor: '#FF0000',
+//     fillOpacity: 0.3,
+//     fillRule: 'evenodd',
+//     className: null,
+//     markerDiv: null,
+//     markerClass: null,
+//     iconAnchor: []
+//   },
+
+//   initialize(geoJSON = this.geoJSON, options = {}) {
+//     // merge default and passed options
+//     this._options = Object.assign({}, this.options, options);
+//     this._geometryType = geoJSON.geometry.type;
+//     this._isMultiPolygon = this._geometryType === 'MultiPolygon';
+
+//     L.Util.setOptions(this, this._options);
+//     this._initGeoJson(geoJSON, this._options);
+//   },
+
+//   _initGeoJson(geoJSON, options) {
+//     this._geoJSONLayer = L.geoJSON(geoJSON, options);
+
+//     // for unique plugin id
+//     this._currentId = id++;
+//   },
+
+//   setCenter(center) {
+//     this._redraw(center.slice(0).reverse());
+//   },
+
+//   reset() {
+//     if (!this._origCenter) {
+//       return false;
+//     }
+
+//     this._redraw(this._origCenter);
+//   },
+
+//   onAdd(map) {
+//     this._map = map;
+//     this._geoJSONLayer.addTo(this._map);
+
+//     // our currentlayer is always the first layer of geoJson layersgroup
+//     // but has a dynamic key
+//     this._currentLayer = this._geoJSONLayer.getLayers()[0];
+//     const centerCoords = this._currentLayer.getCenter();
+//     this._origCenter = [centerCoords.lng, centerCoords.lat];
+
+//     // wrap currentlayer into draggable layer
+//     this._createDraggable(this._currentLayer);
+
+//     this._initialBearingDistance = this._getBearingDistance([
+//       centerCoords.lng,
+//       centerCoords.lat
+//     ]);
+
+//     if (this._options.markerDiv && this._options.markerDiv.length) {
+//       this._dragMarker = this._createMarker(centerCoords, this._options);
+//       this._dragMarker.addTo(this._map);
+//     }
+//   },
+
+//   _createMarker(center, options) {
+//     const { markerClass, markerDiv, iconAnchor } = options;
+//     const dragIcon = L.divIcon({
+//       className: markerClass,
+//       html: markerDiv,
+//       iconAnchor
+//     });
+
+//     return L.marker(center, { icon: dragIcon, draggable: true })
+//       .on('dragstart', this._onMarkerDragStart, this)
+//       .on('drag', this._onMarkerDrag, this);
+//   },
+
+//   _onMarkerDragStart(evt) {
+//     const { lng, lat } = evt.target._latlng;
+//     const center = this._currentLayer.getCenter();
+
+//     this._dragOffset = [lng - center.lng, lat - center.lat];
+//   },
+
+//   _onMarkerDrag(evt) {
+//     this._redraw([evt.latlng.lng, evt.latlng.lat]);
+//   },
+
+//   _createDraggable(layer) {
+//     const draggablePath = new L.Draggable(layer._path);
+//     draggablePath.enable();
+
+//     draggablePath
+//       .on('dragstart', this._onDragStart, this)
+//       .on('drag', this._onDrag, this);
+//   },
+
+//   _onDragStart(evt) {
+//     const event = evt.touches ? evt.touches[0] : evt.target;
+//     const pos = this._getPositionFromEvent(event);
+//     const coords = this._getLatLngFromPosition(pos);
+//     const center = this._currentLayer.getCenter();
+
+//     this._dragOffset = [coords[0] - center.lng, coords[1] - center.lat];
+//   },
+
+//   _onDrag(evt) {
+//     const event = evt.touches ? evt.touches[0] : evt.originalEvent;
+//     const pos = this._getPositionFromEvent(event);
+//     const coords = this._getLatLngFromPosition(pos);
+
+//     this._redraw([
+//       coords[0] - this._dragOffset[0],
+//       coords[1] - this._dragOffset[1]
+//     ]);
+//   },
+
+//   _getPositionFromEvent(evt) {
+//     if (typeof evt._startPoint !== 'undefined') {
+//       return evt._startPoint;
+//     }
+
+//     return { x: evt.clientX, y: evt.clientY };
+//   },
+
+//   _getLatLngFromPosition(pos) {
+//     const { left, top } = this._map._container.getClientRects()[0];
+//     const { x, y } = pos;
+
+//     const posWithOffset = L.point(x - left, y - top);
+//     const { lng, lat } = this._map.containerPointToLatLng(posWithOffset);
+//     return [lng, lat];
+//   },
+
+//   _getBearingDistance(center) {
+//     if (this._isMultiPolygon) {
+//       return this._currentLayer.feature.geometry.coordinates[0].map(coords =>
+//         coords.map(coord => this._getBearingAndDistance(center, coord))
+//       );
+//     }
+
+//     return turfCoordAll(this._currentLayer.feature).map(coord =>
+//       this._getBearingAndDistance(center, coord)
+//     );
+//   },
+
+//   _getBearingAndDistance(center, coord) {
+//     const bearing = turfBearing(center, coord);
+//     const distance = turfDistance(center, coord, { units: 'kilometers' });
+//     return { bearing, distance };
+//   },
+
+//   _redraw(newPos) {
+//     let newPoints;
+
+//     if (this._isMultiPolygon) {
+//       newPoints = this._initialBearingDistance.map(params => [
+//         params.map(param => {
+//           return turfDestination(newPos, param.distance, param.bearing, {
+//             units: 'kilometers'
+//           }).geometry.coordinates;
+//         })
+//       ]);
+//     } else {
+//       newPoints = this._initialBearingDistance.map(param => {
+//         return turfDestination(newPos, param.distance, param.bearing, {
+//           units: 'kilometers'
+//         }).geometry.coordinates;
+//       });
+//     }
+
+//     const newFeature = {
+//       type: 'Feature',
+//       properties: {},
+//       geometry: {
+//         type: this._geometryType,
+//         coordinates: this._getCoordsByType(newPoints, this._geometryType)
+//       }
+//     };
+
+//     this._geoJSONLayer.clearLayers();
+//     this._geoJSONLayer.addData(newFeature);
+//     // our currentlayer is always the first layer of geoJson layersgroup
+//     // but has a dynamic key
+//     this._currentLayer = this._geoJSONLayer.getLayers()[0];
+//     // add draggable hook again, as we using internal a new layer
+//     // center marker if existing
+//     this._createDraggable(this._currentLayer);
+//     this._dragMarker &&
+//       this._dragMarker.setLatLng(this._currentLayer.getCenter());
+//   },
+
+//   onRemove(map) {
+//     this._map = map;
+//     this._map.removeLayer(this._geoJSONLayer);
+//     if (this._dragMarker) {
+//       this._map.removeLayer(this._dragMarker);
+//     }
+
+//     return this;
+//   },
+
+//   _getCoordsByType(point, type) {
+//     switch (type) {
+//       case 'LineString': {
+//         return point;
+//       }
+//       case 'Polygon': {
+//         return [point];
+//       }
+//       case 'MultiPolygon': {
+//         return point;
+//       }
+//       default: {
+//         return [point];
+//       }
+//     }
+//   }
+// });
+
+// L.trueSize = (geoJSON, options) => new L.TrueSize(geoJSON, options);
